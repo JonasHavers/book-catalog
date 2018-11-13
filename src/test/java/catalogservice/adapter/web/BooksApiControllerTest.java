cc425e86fd2ca94a1e4ff6e1191c0efff8222624
Jonas Havers <jh@jonas-havers.de>
2018-11-13 13:32:27 +0100

    Implement BooksApiControllerTest

--- src/test/java/catalogservice/adapter/web/BooksApiControllerTest.java
+++ src/test/java/catalogservice/adapter/web/BooksApiControllerTest.java
@@ -1,22 +1,52 @@
 package catalogservice.adapter.web;
 
+import catalogservice.domain.Book;
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
 import org.junit.Test;
 
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BooksApiControllerTest {
 
-    @Test
-    public void testIndex() throws Exception {
-        try(EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class)) {
-            try(RxHttpClient client = server.getApplicationContext().createBean(RxHttpClient.class, server.getURL())) {
-                assertEquals(HttpStatus.OK, client.toBlocking().exchange("/booksApi").status());
-            }
-        }
+  @Test
+  public void shouldReturnBooks() throws Exception {
+    try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class)) {
+      try (RxHttpClient client = RxHttpClient.create(server.getURL())) {
+        // given
+        HttpRequest<?> request = HttpRequest
+          .GET("/api/books/");
+        // when
+        HttpResponse<List> response = client
+          .toBlocking()
+          .exchange(request, Argument.of(List.class, Book.class));
+        // then
+        HttpStatus responseStatus = response.status();
+        assertEquals(HttpStatus.OK, responseStatus);
+        // and
+        List body = response.body();
+        assertNotNull(body);
+        // and
+        Book book = new Book(
+          "It Doesn't Have to Be Crazy at Work",
+          "Jason Fried",
+          "David Heinemeier Hansson",
+          "0062874780",
+          "9780062874788",
+          240,
+          2018,
+          2018,
+          38900866L
+        );
+        assertTrue(body.contains(book));
+      }
     }
+  }
 }
