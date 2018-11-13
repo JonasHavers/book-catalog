79672642dfd24056224caf7f7703343aea65af22
Jonas Havers <jh@jonas-havers.de>
2018-11-13 13:39:54 +0100

    Implement BooksViewController and add booksView.hbs

--- src/main/java/catalogservice/adapter/web/BooksViewController.java
+++ src/main/java/catalogservice/adapter/web/BooksViewController.java
@@ -1,14 +1,31 @@
 package catalogservice.adapter.web;
 
-import io.micronaut.http.HttpStatus;
+import catalogservice.application.FindBooksUseCase;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.views.ModelAndView;
+import io.micronaut.views.View;
+import io.reactivex.Single;
+import lombok.RequiredArgsConstructor;
 
-@Controller("/booksView")
+import java.util.Map;
+
+@Controller("/")
+@RequiredArgsConstructor
 public class BooksViewController {
+  private final FindBooksUseCase findBooksUseCase;
 
-  @Get("/")
-  public HttpStatus index() {
-    return HttpStatus.OK;
+  @Get
+  @View
+  public Single<ModelAndView> booksView() {
+    return findBooksUseCase.invoke()
+      .toList()
+      .map(bookList -> {
+        Map<String, ?> model = Map.of(
+          "books", bookList,
+          "numberOfBooks", bookList.size()
+        );
+        return new ModelAndView("booksView", model);
+      });
   }
 }
--- src/test/java/catalogservice/adapter/web/BooksViewControllerTest.java
+++ src/test/java/catalogservice/adapter/web/BooksViewControllerTest.java
@@ -6,17 +6,16 @@ import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
 import org.junit.Test;
 
-
 import static org.junit.Assert.assertEquals;
 
 public class BooksViewControllerTest {
 
-    @Test
-    public void testIndex() throws Exception {
-        try(EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class)) {
-            try(RxHttpClient client = server.getApplicationContext().createBean(RxHttpClient.class, server.getURL())) {
-                assertEquals(HttpStatus.OK, client.toBlocking().exchange("/booksView").status());
-            }
-        }
+  @Test
+  public void testBooksView() throws Exception {
+    try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class)) {
+      try (RxHttpClient client = server.getApplicationContext().createBean(RxHttpClient.class, server.getURL())) {
+        assertEquals(HttpStatus.OK, client.toBlocking().exchange("/").status());
+      }
     }
+  }
 }
