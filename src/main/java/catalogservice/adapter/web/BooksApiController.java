16dc701f4e8a2689d0e7ef2b35905cd7c0d21976
Jonas Havers <jh@jonas-havers.de>
2018-11-13 13:30:35 +0100

    Implement port-adapter BooksApiController

--- src/main/java/catalogservice/adapter/web/BooksApiController.java
+++ src/main/java/catalogservice/adapter/web/BooksApiController.java
@@ -1,14 +1,19 @@
 package catalogservice.adapter.web;
 
+import catalogservice.application.FindBooksUseCase;
+import catalogservice.domain.Book;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.HttpStatus;
+import io.reactivex.Flowable;
+import lombok.RequiredArgsConstructor;
 
-@Controller("/booksApi")
+@Controller("/api/books")
+@RequiredArgsConstructor
 public class BooksApiController {
+  private final FindBooksUseCase findBooksUseCase;
 
-    @Get("/")
-    public HttpStatus index() {
-        return HttpStatus.OK;
-    }
+  @Get("/")
+  public Flowable<Book> books() {
+    return findBooksUseCase.invoke();
+  }
 }
